cde7b4a104a0fa84892ceb38d611307d48b53443
Joseph Tarigan <joseph.tarigan@gmail.com>
2018-05-29 16:19:10 +0700

    Change directory position and name

--- CNN/scenario-1/sarcasm-detection-scenario-1.py
+++ CNN/scenario-1/sarcasm-detection-scenario-1.py
@@ -1,2 +1,15 @@
 # Scenario 1
 # Similarity vectors attached with the deep features
+
+import sys
+sys.path.insert(0, 'D:/Workspaces/python/Sarcasm Detector Study')
+from Feature-Extractor import words-vectorizer
+
+one_dimensional_similarity_matrix, two_dimensional_similarity_matrix, one_dimensional_word_vectors, two_dimensional_word_vectors, label_list = word_preprocessing('D:/Workspaces/python/Sarcasm Detector Study/Word2Vec/', 'D:/Workspaces/python/Sarcasm Detector Study/Feature-Extractor/ToadCSVFile_2018-05-23T20_33_112018-05-23 20-33-49.csv', 140, 5, 5, 1000)
+
+# debug
+#print (one_dimensional_similarity_matrix.shape)
+#print (two_dimensional_similarity_matrix.shape)
+#print (one_dimensional_word_vectors.shape)
+#print (two_dimensional_word_vectors.shape)
+#print (len(label_list))
--- Feature-Extractor/words-vectorizer.py
+++ Feature-Extractor/words-vectorizer.py
@@ -83,7 +83,7 @@ def word_preprocessing (w2v_model_path, csv_file_path, word_limit, window_size, 
     return one_dimensional_similarity_matrix, two_dimensional_similarity_matrix, one_dimensional_word_vectors, two_dimensional_word_vectors, label_list
 
 # debug
-#one_dimensional_similarity_matrix, two_dimensional_similarity_matrix, one_dimensional_word_vectors, two_dimensional_word_vectors, label_list = word_preprocessing('D:/Workspaces/python/Sarcasm Detector Study/Word2Vec/', 'D:/Workspaces/python/Sarcasm Detector Study/CNN/Feature Extractor/ToadCSVFile_2018-05-23T20_33_112018-05-23 20-33-49.csv', 140, 5, 5, 1000)
+#one_dimensional_similarity_matrix, two_dimensional_similarity_matrix, one_dimensional_word_vectors, two_dimensional_word_vectors, label_list = word_preprocessing('D:/Workspaces/python/Sarcasm Detector Study/Word2Vec/', 'D:/Workspaces/python/Sarcasm Detector Study/Feature-Extractor/ToadCSVFile_2018-05-23T20_33_112018-05-23 20-33-49.csv', 140, 5, 5, 1000)
 
 
 # debug
